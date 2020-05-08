0665db57c66ef5bc534e0e21eafaf3e342698151
Somesh Chandra <39274935+someshub@users.noreply.github.com>
2020-05-08 20:41:45 +0530

    Add files via upload

---
+++ AiWish/wwwroot/js/chat.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("div");
+    var d2= document.createElement("div");
+    d2.className="d-flex justify-content-end mb-4"
+    d2.setAttribute("name",user);
+    li.className="msg_cotainer_send";
+    li.textContent = msg;
+    d2.appendChild(li);
+    document.getElementById("messagesList").appendChild(d2);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").innerText;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+
